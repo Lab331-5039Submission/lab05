e771371f98df42ecd6a85eb89ea7fe13d72fc1df
Oralita <oat431@gmail.com>
2021-07-29 15:22:28 +0700

    [☕ modified]: the eslintrcc

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,23 +1,23 @@
 <template>
-  <p>Edit data here</p>
-  <button @click="edit">Edit data</button>
+    <p>Edit data here</p>
+    <button @click="edit">Edit data</button>
 </template>
 
 <script>
 export default {
-  props: ['event'],
-  inject: ['GStore'],
-  methods: {
-    edit() {
-      this.GStore.flashMessage ='Data has been updated in ' + this.event.title
-      setTimeout(()=>{
-          this.GStore.flashMessage = ''
-      },3000)
-      this.$router.push({
-        name: 'EventDetails',
-        params: { id: this.event.id }
-      })
-    }
-  }
+    props: ['event'],
+    inject: ['GStore'],
+    methods: {
+        edit() {
+            this.GStore.flashMessage = 'Data has been updated in ' + this.event.title
+            setTimeout(() => {
+                this.GStore.flashMessage = ''
+            }, 3000)
+            this.$router.push({
+                name: 'EventDetails',
+                params: { id: this.event.id },
+            })
+        },
+    },
 }
 </script>
